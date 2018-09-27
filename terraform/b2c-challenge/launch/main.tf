65aaffc0caec7421ba4d7a1ebe55cdee7c21b3f9
adatla <adatla@mapfreusa.com>
2018-09-27 11:07:12 -0400

    removing comments on kubernetes worker nodes

--- terraform/b2c-challenge/launch/main.tf
+++ terraform/b2c-challenge/launch/main.tf
@@ -5,20 +5,19 @@ provider "aws" {
   region     = "${var.aws_region}"
 }
 
-#module "eks_role" {
-#  source = "../modules/kubernetes/eks_iam_roles"
-#}
-
-#module "eks_node" {
-#  source                    = "../modules/kubernetes/eks_node"
-#  eks_cluster_name          = "${var.eks_cluster_name}"
-#  eks_certificate_authority = "${var.eks_certificate_authority}"
-#  eks_endpoint              = "${var.eks_endpoint}"
-#  instance_profile          = "${module.eks_role.iam_instance_profile}"
-#  security_group_id         = "${var.security_group_id}"
-#  subnet_id                 = "${var.subnet_id}"
-#  key_name                  = "${var.key_name}"
-#}
+module "eks_role" {
+  source = "../modules/kubernetes/eks_iam_roles"
+}
+module "eks_node" {
+  source                    = "../modules/kubernetes/eks_node"
+  eks_cluster_name          = "${var.eks_cluster_name}"
+  eks_certificate_authority = "${var.eks_certificate_authority}"
+  eks_endpoint              = "${var.eks_endpoint}"
+  instance_profile          = "${module.eks_role.iam_instance_profile}"
+  security_group_id         = "${var.security_group_id}"
+  subnet_id                 = "${var.subnet_id}"
+  key_name                  = "${var.key_name}"
+}
 
 module "consul_role" {
   source = "../modules/consul/consul_iam_roles"
